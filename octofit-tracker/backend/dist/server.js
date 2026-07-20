32ee5f9cc9f47f0bbbca157dbe8348d7a870951c
amal283 <amal.elsayed43@gmail.com>
2026-07-20 09:16:53 +0000

    Update VITE_CODESPACE_NAME

--- octofit-tracker/backend/dist/server.js
+++ octofit-tracker/backend/dist/server.js
@@ -4,6 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const users_1 = __importDefault(require("./routes/users"));
@@ -20,6 +21,7 @@ const codespaceName = process.env.CODESPACE_NAME;
 const baseUrl = codespaceName
     ? `https://${codespaceName}-8000.app.github.dev`
     : 'http://localhost:8000';
+app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.get('/api/health', (_req, res) => {
     res.json({ status: 'ok', baseUrl });
